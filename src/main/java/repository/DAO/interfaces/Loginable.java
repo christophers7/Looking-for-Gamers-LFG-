42b2b19137f5914166948283ac08d72c936df949
DementedTiger <72563448+DementedTiger@users.noreply.github.com>
2022-01-21 15:06:06 -0600

    interfaces for dao

---
+++ src/main/java/repository/DAO/interfaces/Loginable.java
@@ -0,0 +1,15 @@
+package repository.DAO.interfaces;
+
+import repository.entities.UserCredential;
+
+public interface Loginable {
+
+    int createUser(UserCredential userCredential);
+
+    UserCredential getUser(UserCredential userCredential);
+
+    void updateUser(UserCredential userCredential);
+
+    void deleteUser(UserCredential userCredential);
+
+}
---
+++ src/main/java/repository/DAO/interfaces/Profileable.java
@@ -0,0 +1,15 @@
+package repository.DAO.interfaces;
+
+import repository.entities.UserCredential;
+import repository.entities.UserProfile;
+
+public interface Profileable {
+
+    int createProfile(UserProfile profile);
+
+    UserProfile getUserProfile(UserProfile profile);
+
+    void updateUserProfile(UserProfile profile);
+
+    void deleteUserProfile(UserProfile profile);
+}
