e97fec50daccf5a2f756c52a1ce23703c6eb67d4
Meezy98 <kymon.williams@revature.net>
2022-01-22 18:29:10 -0500

    Fixed entity exceptions

--- src/main/java/repository/entities/test/TestMethods.java
+++
@@ -1,8 +0,0 @@
-package repository.entities.test;
-
-public class TestMethods {
-
-    public static void main(String[] args) {
-        System.out.println(new TestEntity().testProfile());
-    }
-}
