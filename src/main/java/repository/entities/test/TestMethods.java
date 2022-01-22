c03994a929fdd07f7749d78ad1c24aab7b4dc88f
Meezy98 <kymon.williams@revature.net>
2022-01-21 22:09:20 -0500

    Created Entities, setup .cfg,and SessionFactory is good to go as well

---
+++ src/main/java/repository/entities/test/TestMethods.java
@@ -0,0 +1,8 @@
+package repository.entities.test;
+
+public class TestMethods {
+
+    public static void main(String[] args) {
+        System.out.println(new TestEntity().testProfile());
+    }
+}
