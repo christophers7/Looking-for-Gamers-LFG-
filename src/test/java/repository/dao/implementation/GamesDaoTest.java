72b5d4a6de48df2148a6f638210b777f4ffccb3e
Mekha454 <mm6663a@gmail.com>
2022-01-24 21:13:05 -0500

    added test lifecycle

--- src/test/java/repository/dao/implementation/GamesDaoTest.java
+++ src/test/java/repository/dao/implementation/GamesDaoTest.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import repository.DAO.implementation.GamesDao;
 import repository.entities.Games;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class GamesDaoTest {
 	
 	GamesDao gamesDao;
