c3611f15de2774aa0854f1d4b8f91decb4df4d6b
DementedTiger <72563448+DementedTiger@users.noreply.github.com>
2022-02-03 19:58:41 -0600

    steam achievement method was private, great use of time
    Took 42 minutes

--- src/main/java/com/revature/p2_lfg/repository/interfaces/SocialsRepository.java
+++ src/main/java/com/revature/p2_lfg/repository/interfaces/SocialsRepository.java
@@ -6,11 +6,14 @@ import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository("socialsRepository")
 public interface SocialsRepository extends JpaRepository<Socials, SocialId> {
-    @NonNull
+
     @Override
-    Optional<Socials> findById(@NonNull SocialId socialId);
+    Optional<Socials> findById(SocialId socialId);
+
+    List<Socials> findByUserid(int userid);
 }
