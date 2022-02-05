1ba34a4119c64748097685fdb6ef6161876ed546
Mekha454 <mm6663a@gmail.com>
2022-02-05 17:44:45 -0500

    trying to pass tests like I'm back in college

--- LFG/src/app/components/host-group/host-view/host-view.component.spec.ts
+++ LFG/src/app/components/host-group/host-view/host-view.component.spec.ts
@@ -1,12 +1,18 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { SessionStorageService } from 'src/app/_services/sessions/session-storage.service';
+import { TokenStorageService } from 'src/app/_services/user_data/token-storage.service';
+import { UserService } from 'src/app/_services/user_data/user.service';
 
 import { HostViewComponent } from './host-view.component';
 
 describe('HostViewComponent', () => {
   let component: HostViewComponent;
   let fixture: ComponentFixture<HostViewComponent>;
+  let mockSessionStorage : jasmine.SpyObj<SessionStorageService>;
+  let mockTokenStorage : jasmine.SpyObj<TokenStorageService>;
+  let mockUserService : jasmine.SpyObj<UserService>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -24,6 +30,7 @@ describe('HostViewComponent', () => {
   });
 
   it('should create', () => {
+    
    expect(component).toBeTruthy();
   });
  
--- LFG/src/app/components/host-group/user-stats/user-stats.component.spec.ts
+++ LFG/src/app/components/host-group/user-stats/user-stats.component.spec.ts
@@ -1,11 +1,15 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SessionStorageService } from 'src/app/_services/sessions/session-storage.service';
+import { UserService } from 'src/app/_services/user_data/user.service';
 
 import { UserStatsComponent } from './user-stats.component';
 
 describe('UserStatsComponent', () => {
   let component: UserStatsComponent;
   let fixture: ComponentFixture<UserStatsComponent>;
+  let mockUserService : jasmine.SpyObj<UserService>
+  let mockSession : jasmine.SpyObj<SessionStorageService>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -22,6 +26,7 @@ describe('UserStatsComponent', () => {
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    
+    expect(component.getUserStats()).toBeTruthy();
   });
 });
