45e2b27c76c2f3630b6dc92dcf28807845dfce41
Meezy98 <kymon.williams@gmail.com>
2022-02-05 14:16:12 -0500

    Classpath check

--- src/main/java/com/revature/p2_lfg/driver/Application.java
+++ src/main/java/com/revature/p2_lfg/driver/Application.java
@@ -1,9 +1,8 @@
-package com.revature.p2_lfg;
+package com.revature.p2_lfg.driver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
